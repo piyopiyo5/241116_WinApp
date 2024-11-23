e7915b0ab5c5255b54fb44caa80c6aa0a299bbc9
piyopiyo5 <74588735+piyopiyo5@users.noreply.github.com>
2024-11-23 13:34:09 +0900

    コード8.2

--- WpfApp1/WpfApp1/App.xaml.cs
+++ WpfApp1/WpfApp1/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using WpfApp1.ViewModels;
+using WpfApp1.Views;
 
 namespace WpfApp1
 {
@@ -9,6 +11,13 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var w = new MainView();
+            var vm = new MainViewModel();
+            w.DataContext = vm;
+            w.Show();
+        }
     }
-
 }
