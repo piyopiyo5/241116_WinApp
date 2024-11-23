d5fd15373707ac4d9ce2916f540b9e43fb3c3b8d
piyopiyo5 <74588735+piyopiyo5@users.noreply.github.com>
2024-11-23 14:04:41 +0900

    コード8-10

---
+++ WpfApp1/WpfApp1/Models/DelegateCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.Models
+{
+    public class DelegateCommand : ICommand
+    {
+        /// <summary> 
+        /// コマンドの実体を保持します。 
+        /// </summary> 
+        private Action<object> _execute;
+
+        /// <summary> 
+        /// コマンドの実行可能判別処理の実態を保持します。 
+        /// </summary> 
+        private Func<object, bool> _canExecute;
+
+        /// <summary> 
+        /// 新しいインスタンスを生成します。 
+        /// </summary> 
+        /// <param name="execute">コマンドの実体を指定します。</param> 
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary> 
+        /// 新しいインスタンスを生成します。 
+        /// </summary> 
+        /// <param name="execute">コマンドの実体を指定します。</param> 
+        /// <param name="canExecute">コマンドの実行可能判別処理の実体を指定します。</param> 
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        /// <summary> 
+        /// CanExecuteChanged イベントを発行します。
+        ///  /// </summary> 
+        public static void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        #region ICommand のメンバ
+        /// <summary> 
+        /// コマンドの実行可能判別処理を実行します。 
+        /// </summary> 
+        /// <param name="parameter">コマンドパラメータを指定します。</param> 
+        /// <returns>コマンドが実行可能であるとき true を返します。</returns> 
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        /// <summary> 
+        /// コマンドの実行可能判別条件が変更されたときに発生します。 
+        /// </summary> 
+        public event System.EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+
+        /// <summary> 
+        /// コマンドを実行します。 
+        /// </summary> 
+        /// <param name="parameter">コマンドパラメータを指定します。</param> 
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+                _execute(parameter);
+        }
+        #endregion ICommand のメンバ
+    }
+}
--- WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using WpfApp1.Models;
 
 namespace WpfApp1.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged のメンバ 
+        /// <summary> 
+        /// プロパティ変更時に発生します。 
+        /// </summary> 
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion INotifyPropertyChanged のメンバ 
+
+        /// <summary> 
+        /// PropertyChanged イベントを発行します。 
+        /// </summary> 
+        /// <param name="propertyName">プロパティ名を指定します。</param> 
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var h = PropertyChanged;
+            if (h != null)
+                h(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string text;
+        /// <summary> 
+        /// 文字列を取得または設定します。 
+        /// </summary> 
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                if (text != value)
+                {
+                    text = value;
+                    Result = text.ToUpper();
+                    RaisePropertyChanged("Text");
+                }
+            }
+        }
+
+        private string result;
+        /// <summary> 
+        /// 処理結果を取得または設定します。 
+        /// </summary> 
+        public string Result
+        {
+            get { return result; }
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                    RaisePropertyChanged("Result");
+                }
+            }
+        }
+
+        private DelegateCommand clearCommand;
+        /// <summary> 
+        /// 文字列をクリアするコマンドを取得します。 
+        /// </summary> 
+        public DelegateCommand ClearCommand
+        {
+            get
+            {
+                if (clearCommand == null)
+                    clearCommand = new DelegateCommand(
+                    _ => Text = string.Empty,
+                    _ => !string.IsNullOrEmpty(Text)
+                    );
+                return clearCommand;
+            }
+        }
     }
+
 }
