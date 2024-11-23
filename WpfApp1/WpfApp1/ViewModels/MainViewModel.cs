f1a74ef9b421eb9deb216619f0cc8af41021bbda
piyopiyo5 <74588735+piyopiyo5@users.noreply.github.com>
2024-11-23 15:14:21 +0900

    実践コード3-12

--- WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApp1.ViewModels
 {
-    internal class MainViewModel
+    internal class MainViewModel : NotificationObject
     {
+        private string? _upperString;
+        public string UpperString
+        {
+            get { return _upperString; }
+            private set
+            {
+                SetProperty(ref _upperString, value);
+            }
+        }
+
+        private string? _inputString;
+        public string InputString
+        {
+            get { return _inputString; }
+            set
+            {
+                if (SetProperty(ref _inputString, value))
+                {
+                    // 入力された文字列を大文字に変換します 
+                    UpperString = _inputString.ToUpper();
+
+                    // 出力ウィンドウに結果を表示します 
+                    System.Diagnostics.Debug.WriteLine("UpperString=" + UpperString);
+                }
+            }
+        }
     }
 }
