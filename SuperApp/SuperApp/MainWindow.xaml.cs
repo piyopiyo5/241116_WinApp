b5fe9502713c153bb07c9b79fc774bceffa55802
piyopiyo5 <74588735+piyopiyo5@users.noreply.github.com>
2025-03-01 08:34:39 +0900

    ソリューション名、プロジェクト名、アプリ名をSuperAppに変更

--- SuperApp/SuperApp/MainWindow.xaml.cs
+++ SuperApp/SuperApp/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace SuperApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
